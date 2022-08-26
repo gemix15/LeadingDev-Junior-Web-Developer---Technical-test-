d76d839749d1254a40678920744fcd6438402a6f
Gemma Batlle <gembb15@gmail.com>
2022-08-26 13:05:42 +0200

    Adding security

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+
+  http_basic_authenticate_with name: "dev", password: "12345", except: [:index, :show]
+
   def index
     @articles = Article.all
   end
